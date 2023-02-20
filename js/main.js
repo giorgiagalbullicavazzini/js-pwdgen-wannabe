b5b7ad5d1505f98541c850ab0bb7efd6677025fd
giorgiagalbullicavazzini <giorgia.galbullicavazzini@gmail.com>
2023-02-20 14:47:26 +0100

    base text creation

--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@
 // Allo stesso modo, creo anche le variabili lastName e favColor, per chiedergli quali siano il suo cognome e il suo colore preferito;
 // Stampo tutti i risultati in console per verificare di aver fatto il lavoro in modo corretto;
 
+'use strict';
+
 const firstName = prompt('Inserisci il tuo nome');
 const lastName = prompt('Inserisci il tuo cognome');
 const favColor = prompt('Inserisci il tuo colore preferito');
