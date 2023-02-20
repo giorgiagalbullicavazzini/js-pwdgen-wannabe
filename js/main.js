228c56bebdd643ab14fbd9cd8839b75d80194b62
giorgiagalbullicavazzini <giorgia.galbullicavazzini@gmail.com>
2023-02-20 14:41:46 +0100

    obtain data

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-console.log('Hello World!')
+// Infine scrivi sulla pagina e in console nomecognomecolorepreferito89
+
+// README
+// Creo la variabile firstName e, tramite un prompt, chiedo all'utente di fornirmi il suo nome;
+// Allo stesso modo, creo anche le variabili lastName e favColor, per chiedergli quali siano il suo cognome e il suo colore preferito;
+// Stampo tutti i risultati in console per verificare di aver fatto il lavoro in modo corretto;
+
+const firstName = prompt('Inserisci il tuo nome');
+const lastName = prompt('Inserisci il tuo cognome');
+const favColor = prompt('Inserisci il tuo colore preferito');
+
+console.log(`${firstName} ${lastName} ${favColor}`);
