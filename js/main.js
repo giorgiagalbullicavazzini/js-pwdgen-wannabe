602ef2f9315b889802d713724e630917b659629b
giorgiagalbullicavazzini <giorgia.galbullicavazzini@gmail.com>
2023-02-20 15:15:23 +0100

    js data into html

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
-// Infine scrivi sulla pagina e in console nomecognomecolorepreferito89
-
 // README
 // Creo la variabile firstName e, tramite un prompt, chiedo all'utente di fornirmi il suo nome;
 // Allo stesso modo, creo anche le variabili lastName e favColor, per chiedergli quali siano il suo cognome e il suo colore preferito;
 // Creo una quarta variabile, psw, per concatenare i precedenti valori in un'unica password;
 // Stampo il risultato in console per verificare di aver svolto il lavoro nel modo corretto;
+// Seleziono i tag html di mio interesse e vado a modificarli inserendo i dati ricevuti dall'utente.
 
 'use strict';
 
@@ -15,3 +14,6 @@ const favColor = prompt('Inserisci il tuo colore preferito');
 const psw = `${firstName}${lastName}${favColor}89`;
 
 console.log(psw);
+
+document.querySelector('h1').innerHTML = `Ciao, ${firstName} ${lastName}!`;
+document.querySelector('h3').innerHTML = psw;
